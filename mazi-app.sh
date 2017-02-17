8f69c5c54066f2c72a10b4cc1e9dd66f9b01c95f
mavridis <mavridis20880@gmail.com>
2017-02-17 11:54:49 +0000

    mazi-app/mazi-stat scripts

---
+++ mazi-app.sh
@@ -0,0 +1,49 @@
+#!/bin/bash  
+
+#This script control the available applications
+#
+# Usage: sudo sh mazi-app.sh  [options] <application>
+# 
+# [options]
+# -a, --action [start,stop,status] <application>    Set the action and name of application
+#
+
+#set -x
+usage() { echo "Usage: sudo sh mazi-app.sh  [options] <application>" 
+          echo " " 
+          echo "[options]" 
+          echo "-a, --action [start,stop,status] <application>    Set the action and name of application" 1>&2; exit 1; }
+
+
+
+path=$(pwd)
+
+######  Parse command line arguments   ######
+
+key="$1"
+
+case $key in
+    -a| --action)
+     app="$3"
+     case $app in
+         etherpad)
+         sudo service etherpad-lite $2 | grep "Active" |awk '{print $2}'     
+
+         ;;
+         mazi-board)
+         sudo service mazi-board $2 | grep "Active" |awk '{print $2}'
+         ;;
+         *)
+         # unknown option
+         echo "This application is not available"   
+         ;;
+    esac
+    
+    ;;
+    *)
+       # unknown option
+    usage   
+    ;;
+esac
+
+ 
---
+++ mazi-stat.sh
@@ -0,0 +1,44 @@
+#!/bin/bash  
+
+#This script displays the total online users in the local network 
+#
+# Usage: sudo sh mazi-stat.sh  [options]
+# 
+# [options]
+# -u,--users     Display online users 
+#
+
+#set -x
+usage() { echo "Usage: sudo sh mazi-stat.sh  [options]" 
+          echo " " 
+          echo "[options]" 
+          echo "-u,--users        Display the total online users" 1>&2; exit 1; }
+
+
+
+######  Parse command line arguments   ######
+path=$(pwd)
+key="$1"
+
+case $key in
+    -u|--users)
+    cd $path
+    sudo touch users.log
+    sudo chmod 777 users.log 
+    sudo arp-scan --interface=wlan0 10.0.0.1/24 --retry=3 -g  > users.log
+    users=$(cat users.log | grep 'responded' | awk '{print $12}')
+    echo "wifi users" $users 
+    ;;
+    *)
+       # unknown option
+    usage   
+    ;;
+esac
+exit 0
+
+
+#set +x
+
+
+
+
