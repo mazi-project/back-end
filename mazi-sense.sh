af12e752234bc0090e3b935eada7dcdf06bbf01c
root <root@mazizone>
2017-05-16 19:08:58 +0300

    new directory for Type

--- mazi-sense.sh
+++ mazi-sense.sh
@@ -37,7 +37,8 @@ usage() { echo "Usage: sudo sh mazi-sense.sh [SenseName] [Options] [SensorOption
 	  echo "-h , --humidity                     Get the Humidity" 1>&2; exit 1; }
 DUR="0"
 INT="0"
-path=$(pwd)
+path_sense="$(pwd)/lib"
+path_type="/etc/mazi"
 while [ $# -gt 0 ]
 do
    key="$1"
@@ -72,8 +73,12 @@ do
    shift     #past argument or value
 done
 
-endTime=$(( $(date +%s) + $DUR )) # Calculate end time.
+#### create  a Type file #####
+if [ ! -f "$path_type/Type" ]; then
+    sudo touch $path_type/Type
+fi
 
+endTime=$(( $(date +%s) + $DUR )) # Calculate end time.
 
 while [ true ]; do
 
@@ -89,8 +94,8 @@ while [ true ]; do
    ##### STH11 Sensor #####
    if [ "$NAME" = "sth11"  ];then
       echo "$NAME"
-      temp="$(python $path/lib/sth11.py $TEMP)"
-      hum="$(python $path/lib/sth11.py $HUM)"
+      temp="$(python $path_sense/sth11.py $TEMP)"
+      hum="$(python $path_sense/sth11.py $HUM)"
       if [ $TEMP ]; then
          echo "The Temperature is: $temp"
       fi
@@ -100,11 +105,10 @@ while [ true ]; do
       ##### STORE OPTION #####
       if [ $STORE ]; then
          #### Take the ID of sensor ####
-         cd $path
-         ID=$(cat Type | grep "$NAME" | awk '{print $2}')   #Search for id that corresponds to the name of the sensor
+         ID=$(cat $path_type/Type | grep "$NAME" | awk '{print $2}')   #Search for id that corresponds to the name of the sensor
          if [ ! $ID ]; then                                   #If ID doesn't exist, get the ID through the restAPI
             ID=$(curl -s -X POST  http://portal.mazizone.eu/sensors/register/$NAME)
-            sudo echo "$NAME  $ID" >> $path/Type
+            sudo echo "$NAME  $ID" >> $path_type/Type
          fi
          TIME=$(date  "+%H%M%S%d%m%y")
          curl --data '{"sensor_id":'$ID',"value":{"temp":'$temp',"hum":'$hum'},"date":"'$TIME'"}' http://portal.mazizone.eu/sensors/store
