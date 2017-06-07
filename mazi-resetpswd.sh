196a97cbeb167fced938f538d3cb3ac33ef9e64c
mavridis92 <giamavridis@gmail.com>
2017-06-07 18:19:25 +0300

    output message

--- mazi-resetpswd.sh
+++ mazi-resetpswd.sh
@@ -18,4 +18,4 @@ fi
 sed -i "/:admin_password:/c\  :admin_password: '1234'" /etc/mazi/config.yml
 sudo service mazi-portal restart
 echo ""
-echo "You have just reset your password, please go to the admin page and change your password"
+echo "You have just reset your password to 1234, please go to the admin page and change it"
