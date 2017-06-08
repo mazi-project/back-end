ddad443931d9f6c05b95bb5e801bbd7288bec40c
root <root@mazizone>
2017-06-08 12:21:01 +0300

    New domain

--- mazi-domain.sh
+++ mazi-domain.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-#This script changes the domain of 
+#This script changes the domain of mazi toolkit
 #
-# Usage: mazi-resetpswd.sh
+# Usage: mazi-domain.sh -d <new domain>
 
 
 usage() { echo "This script changes the domain of mazi toolkit"
           echo ""
           echo "Usage: mazi-domain.sh -d <new domain> "  1>&2; exit 1; }
 
-if [ $# -gt 0 ];then
-   # unknown option
-   usage
-fi
+while [ $# -gt 0 ]
+do
+key="$1"
+case $key in
+    -d)
+    domain="$2"
+    shift # past argument=value
+    ;;
+    *)
+    # unknown option
+    usage
+    ;;
+esac
+shift     #past argument or value
+done
 
 
-sed -i "//c\  :admin_password: '1234'" /etc/mazi/config.yml
-
+sed -i '/New Domain/,/EOF/d' /etc/hosts
+echo "New Domain" >> /etc/hosts
+echo "10.0.0.1        $domain" >> /etc/hosts
+sed -i '/<meta HTTP-EQUIV="REFRESH"/c\          <meta HTTP-EQUIV="REFRESH" content="0; url=http://'$domain'">' /var/www/html/index.html
+sed -i "/ServerName/c\        ServerName $domain" /etc/apache2/sites-available/portal.conf 
+sudo systemctl daemon-reload
+sudo service apache2 restart
