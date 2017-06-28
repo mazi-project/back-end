95e92ad992a2089a2e5694fe73c28deb6a7b8dcd
mavridis <giamavridis@gmail.com>
2017-06-28 12:33:27 +0300

    restart dnsmasq

--- mazi-domain.sh
+++ mazi-domain.sh
@@ -27,9 +27,10 @@ done
 
 
 sed -i '/New Domain/,/EOF/d' /etc/hosts
-echo "New Domain" >> /etc/hosts
+echo "#New Domain" >> /etc/hosts
 echo "10.0.0.1        $domain" >> /etc/hosts
 sed -i '/<meta HTTP-EQUIV="REFRESH"/c\          <meta HTTP-EQUIV="REFRESH" content="0; url=http://'$domain'">' /var/www/html/index.html
 sed -i "/ServerName/c\        ServerName $domain" /etc/apache2/sites-available/portal.conf 
 sudo systemctl daemon-reload
 sudo service apache2 restart
+sudo service dnsmasq restart
