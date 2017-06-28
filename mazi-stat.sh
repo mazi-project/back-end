a7228a9607c16321500b94b7c4d13f16358b2e8b
mavridis <giamavridis@gmail.com>
2017-06-28 13:49:52 +0300

    router update

--- mazi-stat.sh
+++ mazi-stat.sh
@@ -25,14 +25,25 @@ usage() { echo "Usage: sudo sh mazi-stat.sh  [options]"
 path=$(pwd)
 key="$1"
 
+if [ "$(sh current.sh -w)" = "device OpenWrt router" ];then
+   ROUTER="TRUE"
+fi
+
+
 case $key in
     -u|--users)
     cd $path
     sudo touch users.log
     sudo chmod 777 users.log 
-    sudo arp-scan --interface=wlan0 10.0.0.1/24 --retry=3 -g  > users.log
-    users=$(cat users.log | grep 'responded' | awk '{print $12}')
-    echo "wifi users" $users 
+    if [ "$ROUTER" ];then
+      sudo arp-scan --interface=eth0 10.0.2.1/24 --retry=3 -g  > users.log
+      users=$(cat users.log | grep 'responded' | awk '{print $12}') 
+      echo "wifi users $(($users - 1))"
+    else
+      sudo arp-scan --interface=wlan0 10.0.0.1/24 --retry=3 -g  > users.log
+      users=$(cat users.log | grep 'responded' | awk '{print $12}')
+      echo "wifi users" $users    
+    fi
     ;;
     -t|--temp)
     vcgencmd measure_temp
