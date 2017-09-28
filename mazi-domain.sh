70ae313fd609ed75fbc8ee8307a8b8bffd0acb14
mavridis <giamavridis@gmail.com>
2017-09-28 17:00:56 +0300

    splash functionality

--- mazi-domain.sh
+++ mazi-domain.sh
@@ -4,13 +4,17 @@
 #
 # Usage: mazi-domain.sh -d <new domain>
 
-set -x
+###### Initialization ######
+hosts="/etc/hosts"
+portal_conf="/etc/apache2/sites-available/portal.conf"
+index_apache="/var/www/html/index.html"
+
 usage() { echo "This script changes the domain of mazi toolkit"
           echo ""
           echo "Usage: mazi-domain.sh [options]"
-          echo "[options]"          
-          echo "-d,--domain  <new domain>   Set a new Domail of portal page"
-          echo "-s,--splash  <application>  Set the application as a basic page"  1>&2; exit 1; }
+          echo "[options]"
+          echo "-d,--domain  <new domain>             Set a new Domail of portal page"
+          echo "-s,--splash  <application>/<portal>   Set a application or portal as a basic page"  1>&2; exit 1; }
 
 while [ $# -gt 0 ]
 do
@@ -32,40 +36,80 @@ esac
 shift     #past argument or value
 done
 
+
 if [ $domain ];then
-  sed -i '/New Domain/,/EOF/d' /etc/hosts
-  echo "#New Domain" >> /etc/hosts
-  echo "10.0.0.1        $domain" >> /etc/hosts
-  sed -i '/<meta HTTP-EQUIV="REFRESH"/c\          <meta HTTP-EQUIV="REFRESH" content="0; url=http://'$domain'">' /var/www/html/index.html
-  sed -i "/ServerName/c\        ServerName $domain" /etc/apache2/sites-available/portal.conf 
+  c_domain=$(grep -o -P '(?<=url=http://).*(?=">)' $index_apache)
+  sed -i "s/$c_domain/$domain/g" $hosts
+  sed -i '/<meta HTTP-EQUIV="REFRESH"/c\          <meta HTTP-EQUIV="REFRESH" content="0; url=http://'$domain'">' $index_apache
+  sed -i "s/$c_domain/$domain/g" $portal_conf 
   sudo systemctl daemon-reload
   sudo service apache2 restart
   sudo service dnsmasq restart
-
 fi
 
 if [ $app ];then
-  case $app in
-    guestbook)
-    domain="local.mazizone.eu:8081"
+   domain=$(grep -o -P '(?<=url=http://).*(?=">)' $index_apache)
+   case $app in
+    guestbook | etherpad)
+    
+    [ "$app" = "guestbook" ] && port="8081" || port="9001"
+    cat << EOF >  $portal_conf
+<VirtualHost *:80>
+        Redirect /admin http://local.mazizone.eu/admin
+        ServerName $domain
+        ProxyPreserveHost On
+        ProxyRequests Off
+        ProxyPass  /admin !
+        ProxyPass / http://localhost:$port/
+        ProxyPassReverse / http://localhost:$port/
+
+</VirtualHost>
+
+<VirtualHost *:80>
+        ServerName local.mazizone.eu
+        ProxyPreserveHost On
+        ProxyRequests Off
+        ProxyPass / http://localhost:4567/
+        ProxyPassReverse / http://localhost:4567/
+
+</VirtualHost>
+EOF
     ;;
-    etherpad)
-    domain="local.mazizone.eu:9001"
+    framadate| nextcloud | wordpress)    
+    
+    cat << EOF > $portal_conf
+<VirtualHost *:80>
+        Redirect /admin http://local.mazizone.eu/admin
+        DocumentRoot /var/www/html/$app
+        ServerName $domain
+</VirtualHost>
+
+<VirtualHost *:80>
+        ServerName local.mazizone.eu
+        ProxyPreserveHost On
+        ProxyRequests Off
+        ProxyPass / http://localhost:4567/
+        ProxyPassReverse / http://localhost:4567/
+</VirtualHost>
+EOF
     ;;
-    framadate)
-    domain="local.mazizone.eu/framadate"
-    ;;
-    nextcloud)
-    domain="local.mazizone.eu/nextcloud"
+    portal)
+    cat << EOF > $portal_conf
+<VirtualHost *:80>
+        ServerName $domain
+        ProxyPreserveHost On
+        ProxyRequests Off
+        ProxyPass / http://localhost:4567/
+        ProxyPassReverse / http://localhost:4567/
+</VirtualHost>
+EOF
     ;;
     *)
-    echo "unavailable application"
+    echo "This application is not available "
     ;;
   esac
-  sed -i '/<meta HTTP-EQUIV="REFRESH"/c\          <meta HTTP-EQUIV="REFRESH" content="0; url=http://'$domain'">' /var/www/html/index.html
+
   sudo systemctl daemon-reload
   sudo service apache2 restart
-
 fi
-
-set +x
+exit 0
