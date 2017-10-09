8332faafcf53c837b1829e140a9ab58ee4d05b11
mavridis <giamavridis@gmail.com>
2017-10-09 11:27:09 +0300

    update mazi-etherpad

--- mazi-etherpad.sh
+++ mazi-etherpad.sh
@@ -4,16 +4,17 @@ usage() {
    echo "Usage: sudo sh mazi-etherpad.sh  [options]" 
    echo " " 
    echo "[options]"
-   echo "--store           [enable] or [disable ]" 1>&2; exit 1;
+   echo "--store           [enable] or [disable ]" 
+   echo "-d,--domain       Set a remote server domain.( Default is localhost )" 1>&2; exit 1;
 }
 
 data_fun() {
-   users=$(mysql -uroot -pm@z1 etherpad -e 'select store.value from store' |grep -o '"padIDs":{".*":.*}}' | wc -l)
+   users=$(mysql -u$username -p$password etherpad -e 'select store.value from store' |grep -o '"padIDs":{".*":.*}}' | wc -l)
 
-   pads="$(mysql -uroot -pm@z1 etherpad -e 'select store.key from store' |grep -Eo '^pad:[^:]+' |sed -e 's/pad://' |sort |uniq -c |sort -rn |awk '(count+=1) {if ($1!="2") { print count}}' |tail -1)"
+   pads=$(mysql -u$username -p$password etherpad -e 'select store.key from store' |grep -Eo '^pad:[^:]+' |sed -e 's/pad://' |sort |uniq -c |sort -rn |awk '(count+=1) {if ($1!="2") { print count}}' |tail -1)
 
    datasize=$(echo "SELECT ROUND(SUM(data_length + index_length), 2)  as Size_in_B FROM information_schema.TABLES 
-          WHERE table_schema = 'etherpad';" | mysql -uroot -pm@z1)
+          WHERE table_schema = 'etherpad';" | mysql -u$username -p$password)
    datasize=$(echo $datasize | awk '{print $NF}')
   
    TIME=$(date  "+%H%M%S%d%m%y")
@@ -28,11 +29,19 @@ data_fun() {
 ##### Initialization ######
 conf="/etc/mazi/mazi.conf"
 interval="10"
+domain="localhost"
+ #Database
+username=$(jq -r ".username" /etc/mazi/sql.json)
+password=$(jq -r ".password" /etc/mazi/sql.json)
 
 key="$1"
 case $key in
       --store)
       store="$2"
+      shift
+      ;;
+      -d|--domain)
+      domain="$2"
       shift
       ;;
       *)
@@ -43,15 +52,15 @@ esac
 
 
 if [ $store ];then
-  id=$(curl -s -X GET -d @$conf http://10.0.0.1:4567/device/id)
-  [ ! $id ] && id=$(curl -s -X POST -d @$conf http://10.0.0.1:4567/deployment/register)
-  curl -s -X POST --data '{"deployment":'$(jq ".deployment" $conf)'}' http://10.0.0.1:4567/create/etherpad
+  id=$(curl -s -X GET -d @$conf http://$domain:4567/device/id)
+  [ ! $id ] && id=$(curl -s -X POST -d @$conf http://$domain:4567/deployment/register)
+  curl -s -X POST --data '{"deployment":'$(jq ".deployment" $conf)'}' http://$domain:4567/create/etherpad
 
   if [ $store = "enable" ];then
      while [ true ]; do
        target_time=$(( $(date +%s)  + $interval ))
        data_fun
-       curl -s -X POST --data "$data" http://10.0.0.1:4567/update/etherpad
+       curl -s -X POST --data "$data" http://$domain:4567/update/etherpad
        current_time=$(date +%s)
        sleep_time=$(( $target_time - $current_time ))
        [ $sleep_time -gt 0 ] && sleep $sleep_time
