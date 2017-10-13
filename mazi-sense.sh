f7ec0a10038a16c766d729c6b2e236bb0f65515f
mavridis <giamavridis@gmail.com>
2017-10-13 18:01:57 +0300

    update mazi-sense for v2

--- mazi-sense.sh
+++ mazi-sense.sh
@@ -3,31 +3,6 @@
 #This script manages all available  sensors 
 #
 # Usage: sudo sh mazi-sense.sh [senseName] [options]
-#
-# 
-#
-# [senseName]
-# -n,--name                       Set the name of the sensor
-#
-# [options]
-# -s , --store                    Store the measurements to Database through the restAPI
-# -d , --duration                 Duration in seconds to take a measurement
-# -i , --interval                 Seconds between periodic measurement
-# -a , --available                Displays the status of the available sensors
-# --init                          Sensor initialization
-#
-#
-# [sht11]
-# -t , --temporature              Displays the Temporature 
-# -h , --humidity                 Displays the Humidity 
-#
-# [sensehat]
-# -t , --temporature              Displays the Temporature 
-# -h , --humidity                 Displays the Humidity 
-#
-#set -x
-#
-
 
 usage() { echo "Usage: sudo sh mazi-sense.sh [SenseName] [Options] [SensorOptions]"
 	  echo ""
@@ -51,11 +26,14 @@ usage() { echo "Usage: sudo sh mazi-sense.sh [SenseName] [Options] [SensorOption
           echo "  -t , --temperature                 Get the Temperature "
           echo "  -h , --humidity                    Get the Humidity" 1>&2; exit 1; }
 
+##### Initialization  ######
 DUR="0"     #initialization of duration 
-INT="0"     #initialization of interval 
+interval="0"     #initialization of interval 
+domain="localhost"
+conf="/etc/mazi/mazi.conf"
 path_sense="/root/back-end/lib"
-path_Type="/etc/mazi"
 IP="$(ifconfig wlan0 | grep 'inet addr' | awk '{printf $2}'| grep -o '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*')"
+
 while [ $# -gt 0 ]
 do
    key="$1"
@@ -79,7 +57,7 @@ do
         shift
         ;;
         -i|--interval)
-        INT="$2"
+        interval="$2"
         shift
         ;;
         -a|--available)
@@ -97,27 +75,7 @@ do
    shift     #past argument or value
 done
 
-
-##### Initialization  ######
-
-if [ $INITPSW ]; then
-	echo "Installing sense-hat"
-	apt-get install -y -f sense-hat
-	echo "Configuring SQL database"
-	#mysql -u root -p$INITPSW -e "CREATE DATABASE IF NOT EXISTS sensors;"
-	mysql -u root -p$INITPSW -e "CREATE DATABASE IF NOT EXISTS sensors;"
-	mysql -u root -p$INITPSW -e "CREATE USER mazi_user@localhost IDENTIFIED BY '1234';"
-	mysql -u root -p$INITPSW -e "GRANT ALL ON sensors.* TO 'mazi_user'@'localhost' IDENTIFIED BY '1234';"
-	mysql -u root -p$INITPSW -e "FLUSH PRIVILEGES;"
-	mysql -u root -p$INITPSW -e "CREATE TABLE IF NOT EXISTS sensors.type (id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(10), ip VARCHAR(15));"
-	echo "Sensor Initialization finished"
-
-	exit 0;
-fi
-
-
 ##### Scan for available sensors  ######
-
 if [ $SCAN ];then
    if [ -f "/proc/device-tree/hat/product" ]; then
       SERVICE="$(ps -ef | grep sensehat | grep -v 'grep')"
@@ -139,62 +97,59 @@ if [ ! $NAME ];then
     exit 0;
 fi
 
-endTime=$(( $(date +%s) + $DUR )) # Calculate end time.
-
 ##### Register the ID of sensor #####
 if [ $STORE ]; then
    #### Take the ID of sensor ####
-   ID="$(curl -s -X GET --data '{"name":"'$NAME'","ip":"'$IP'"}' http://10.0.0.1:4567/sensors/id)" #Search for id that corresponds to the name
-                                                                                                   # and ip of the sensor
-   if [ ! $ID ]; then               #If ID doesn't exist,register the sensor and get the ID  
-      ID="$(curl -s -X POST --data '{"name":"'$NAME'","ip":"'$IP'"}' http://10.0.0.1:4567/sensors/register)" 
+   ID=$(curl -s -X GET --data '{"deployment":'$(jq ".deployment" $conf)',"sensor_name":"'$NAME'","ip":"'$IP'"}' http://$domain:4567/sensors/id) 
+   
+   if [ ! $ID ];then
+      device_id=$(curl -s -X GET -d @$conf http://$domain:4567/device/id)
+      [ ! $device_id ] && device_id=$(curl -s -X POST -d @$conf http://$domain:4567/deployment/register)
+      ID=$(curl -s -X POST --data '{"deployment":'$(jq ".deployment" $conf)',"sensor_name":"'$NAME'","ip":"'$IP'","device_id":"'$device_id'"}' http://$domain:4567/sensor/register)
    fi
+   curl -s -X POST --data '{"deployment":'$(jq ".deployment" $conf)'}' http://$domain:4567/create/$NAME
 fi
+
+endTime=$(( $(date +%s) + $DUR )) # Calculate end time.
+case $NAME in
+   sensehat)
+   
+   while [ true ]; do
+     echo "$NAME"
+     temp="$(python $path_sense/$NAME.py $TEMP)"
+     hum="$(python $path_sense/$NAME.py $HUM)"
+     [ $TEMP ] && echo "The Temperature is: $temp"
+     [ $HUM ] &&  echo "The Humidity is: $hum"
+     ##### STORE OPTION #####
+     if [ $STORE ]; then
+        TIME=$(date  "+%H%M%S%d%m%y")
+        curl -X POST --data '{"deployment":'$(jq ".deployment" $conf)',"sensor_id":'$ID',"value":{"temp":'$temp',"hum":'$hum'},"date":'$TIME'}' http://$domain:4567/update/$NAME
+     fi
+     sleep $interval
+     [ $(date +%s) -ge $endTime ] && exit 0; 
+   done
+   ;;
+   sht11)
+
+   while [ true ]; do
+     echo "$NAME"
+     temp="$(python $path_sense/$NAME.py $TEMP)"
+     hum="$(python $path_sense/$NAME.py $HUM)"
+     [ $TEMP ] && echo "The Temperature is: $temp"
+     [ $HUM ] && echo "The Humidity is: $hum"
+     ##### STORE OPTION #####
+     if [ $STORE ]; then
+        TIME=$(date  "+%H%M%S%d%m%y")
+        curl -X POST --data '{"deployment":'$(jq ".deployment" $conf)',"sensor_id":'$ID',"value":{"temp":'$temp',"hum":'$hum'},"date":'$TIME'}' http://$domain:4567/update/$NAME
+     fi
+     sleep $interval
+     [ $(date +%s) -ge $endTime ] && exit 0; 
+   done
+   ;;
+   *)
  
-while [ true ]; do
-
-   ##### SHT11 Sensor #####
-   if [ "$NAME" = "sht11"  ];then
-      echo "$NAME"
-      temp="$(python $path_sense/$NAME.py $TEMP)"
-      hum="$(python $path_sense/$NAME.py $HUM)"
-      if [ $TEMP ]; then
-         echo "The Temperature is: $temp"
-      fi
-      if [ $HUM ]; then
-         echo "The Humidity is: $hum"
-      fi
-      ##### STORE OPTION #####
-      if [ $STORE ]; then
-         TIME=$(date  "+%H%M%S%d%m%y")
-         curl -X POST --data '{"sensor_id":'$ID',"value":{"temp":'$temp',"hum":'$hum'},"date":'$TIME'}' http://10.0.0.1:4567/sensors/store
-      fi
-   fi
-
-    ##### SenseHat Sensor #####
-   if [ "$NAME" = "sensehat" ];then
-      echo "$NAME"
-      temp="$(python $path_sense/$NAME.py $TEMP)"
-      hum="$(python $path_sense/$NAME.py $HUM)"
-      if [ $TEMP ]; then
-         echo "The Temperature is: $temp"
-      fi
-      if [ $HUM ]; then
-         echo "The Humidity is: $hum"
-      fi
-      ##### STORE OPTION #####
-      if [ $STORE ]; then
-         TIME=$(date  "+%H%M%S%d%m%y")
-         curl -X POST --data '{"sensor_id":'$ID',"value":{"temp":'$temp',"hum":'$hum'},"date":'$TIME'}' http://10.0.0.1:4567/sensors/store
-      fi
-   fi
-
-   #### Exit Statement ####
-   sleep $INT
-   if [ $(date +%s) -ge $endTime ]; then
-      exit 0  
-   fi
-done
-
-#set +x
-
+  echo "Wrong name of sensor"
+   echo ""
+   usage
+   ;;
+esac
