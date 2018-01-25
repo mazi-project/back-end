b83c4cd64af7edb8f5610cf77d50a2721c958e5b
mavridis <giamavridis@gmail.com>
2018-01-25 17:05:32 -0200

    fix status

--- mazi-appstat.sh
+++ mazi-appstat.sh
@@ -112,9 +112,9 @@ status_call() {
     response=$(tac /etc/mazi/rest.log| grep "$1" | awk -v FS="($1:|http_code:)" '{print $2}')
     http_code=$(tac /etc/mazi/rest.log| grep "$1" | head -1 | awk '{print $NF}')
   fi
-  [ "$http_code" = "200" -a "$response" = " OK " ] && call_st="OK" && error=""
+  [ "$http_code" = "200" -a "$(echo $response | grep "OK")"  ] && call_st="OK" && error=""
   [ "$http_code" = "000" ] && call_st="ERROR:" && error="Connection refused"
-  [ "$http_code" = "200" -a "$response" != " OK " ] && call_st="ERROR:" && error="$response"
+  [ "$http_code" = "200" -a ! "$(echo $response | grep "OK")" ] && call_st="ERROR:" && error="$response"
   [ "$http_code" = "500" ] && call_st="ERROR:" && error="The server encountered an unexpected condition which prevented it from fulfilling the request."
 
 }
