2a4e138f9f0d972dd564bcacff4e7a5d47887258
mavridis <giamavridis@gmail.com>
2018-07-25 15:35:42 +0300

    nothing changeinternet.sh

---
+++ timeout.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+filename="/etc/mazi/users.dat"
+nodogMinutes=$(cat /etc/nodogsplash/nodogsplash.conf | grep "ClientIdleTimeout" | awk {'print $NF'})
+while true; 
+do
+  while read -r line
+  do
+    current=$(echo $line | awk '{$1="";print $0}') 
+    mac=$(echo $line | awk '{print $1}')
+    CURRENT=$(date +%s -d "$current")
+    target=$(date "+%F %T %z")
+    TARGET=$(date +%s -d "$target")
+    MINUTES=$(( ($TARGET - $CURRENT) / 60 ))
+
+    if [ $MINUTES -ge $nodogMinutes ];then 
+      sed -i "/$line/d" $filename
+      ndsctl deauth $mac >/dev/null
+      ndsctl untrust $mac >/dev/null
+    fi
+    done < "$filename"
+    sleep 60;
+done
