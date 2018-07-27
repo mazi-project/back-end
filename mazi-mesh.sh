ad8fbd00181cf9d16e6cf458887f7f11a1bb0f4e
mavridis <giamavridis@gmail.com>
2018-07-27 12:31:26 +0300

    The node sends identity information to the gateway

--- mazi-mesh.sh
+++ mazi-mesh.sh
@@ -3,6 +3,8 @@
 #set -x
 
 ## initialization ##
+gateway="192.168.1.1"
+port="7654"
 conf="/etc/mazi/mazi.conf"
 wifi_intface=$(bash mazi-current.sh -i wifi | awk '{print $2}')
 internet_intface=$(bash mazi-current.sh -i internet | awk '{print $2}')
@@ -22,7 +24,22 @@ usage() { echo "Usage: sudo bash mazi-mesh.sh [Mode] [Options]"
           echo "  -i, --interface              Set the interface of the mesh network"
           echo "  -s, --ssid                   Set the name of the mesh network" 1>&2; exit 1; 
 }
-
+register_node(){
+  ip=$(ifconfig br0 | grep 'inet addr' | awk '{printf $2}'| grep -o '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*')
+  ssd=$(bash /root/back-end/mazi-current.sh -s |awk {'print $NF'})
+  data='{"deployment":'$(jq ".deployment" $conf)',
+         "ssid":"'$ssd'",
+         "admin":'$(jq ".admin" $conf)',
+         "title":'$(jq ".title" $conf)',
+         "description":'$(jq ".description" $conf)',
+         "loc":'$(jq ".loc" $conf)'}'
+  node_id=$(curl -s -X POST --data "$data" http://$gateway:$port/register/node/information)
+  if [ ! $node_id ];then
+     curl -s -X POST  http://$gateway:$port/create/mesh
+     node_id=$(curl -s -X POST --data "$data" http://$gateway:$port/register/node/information)
+  fi
+  curl -s -P POST -d '{"node_id":"'$node_id'","ip":"'$ip'"}' http://$gateway:$port/register/node
+}
 batIface(){
   ifaces=$(netstat -i |  awk '{print $1}' | grep -v "Kernel" | grep -v "Iface")
   read -a ifaces <<<$ifaces
@@ -99,7 +116,7 @@ node(){
   sudo dhclient  br0
   bash mazi-wifi.sh restart
   echo $(cat $conf | jq '.+ {"mesh": "node"}') | sudo tee $conf
- 
+  register_node 
 }
 portal(){
  cd /root/back-end
@@ -190,7 +207,6 @@ case $1 in
      usage
      ;;
 esac
-
 
 
 #set +x
