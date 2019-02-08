3ef7d6ed6bc9ba63d3e23ff17e0c60e72f978a11
mavridis <giamavridis@gmail.com>
2019-02-08 10:15:23 +0000

    fix if statement

--- mazi-users.sh
+++ mazi-users.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $2 == "AP-STA-CONNECTED" ]];then
  	name=""
-	while [ -z $name ]; do 
+	while [ -z "$name" ]; do 
 	    ip=$(cat /var/lib/misc/dnsmasq.leases | grep $3 | awk {'print $3'})
 		name=$(cat /var/lib/misc/dnsmasq.leases | grep $3 | awk {'print $4'})
 	done
