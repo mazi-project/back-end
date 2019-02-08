7397c42ef0b807a8ca9a9f3baa748a5fb7a4b43d
mavridis <giamavridis@gmail.com>
2019-02-08 14:19:43 +0000

    double check

--- mazi-users.sh
+++ mazi-users.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [[ $2 == "AP-STA-CONNECTED" ]];then
  	name=""
-	while [ -z "$name" ]; do 
+    ip=""
+	while [[ -z "$name" ]] && [[ -z $ip ]]; do 
 	    ip=$(cat /var/lib/misc/dnsmasq.leases | grep $3 | awk {'print $3'})
 		name=$(cat /var/lib/misc/dnsmasq.leases | grep $3 | awk {'print $4'})
 	done
