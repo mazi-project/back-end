8741381c42f6db3040a15bdaf19bdf12f8333010
mavridis <giamavridis@gmail.com>
2019-02-12 09:03:58 +0000

    remove duplicate users

--- mazi-users.sh
+++ mazi-users.sh
@@ -6,6 +6,7 @@ if [[ $2 == "AP-STA-CONNECTED" ]];then
 	    ip=$(cat /var/lib/misc/dnsmasq.leases | grep $3 | awk {'print $3'})
 		name=$(cat /var/lib/misc/dnsmasq.leases | grep $3 | awk {'print $4'})
 	done
+	sed -i "/$3/d" /etc/mazi/users.log
 	echo "$3 $ip $name" >> /etc/mazi/users.log
 fi
 if [[ $2 == "AP-STA-DISCONNECTED" ]];then
