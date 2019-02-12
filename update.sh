c594e426c3a1b46b183a563df6382324673e5444
mavridis <giamavridis@gmail.com>
2019-02-12 11:07:06 +0000

    delete old users at boot time

--- update.sh
+++ update.sh
@@ -140,8 +140,10 @@ users_count(){
     sed -i "/wmm_ac_vo_acm=0/a # Hostapd_cli configuration" /etc/hostapd/hostapd.conf
     sed -i "/# Hostapd_cli configuration/a ctrl_interface=/var/run/hostapd" /etc/hostapd/hostapd.conf
     sed -i "/ctrl_interface=\/var\/run\/hostapd/a ctrl_interface_group=0" /etc/hostapd/hostapd.conf	
-    sed "/echo \$(cat \/etc\/mazi\/mazi.conf | jq /a #RESTART MAZI-USERS SERVICE" /etc/rc.local
-    sed -i "/#RESTART MAZI-USERS SERVICE/a /etc/init.d/mazi-users restart" /etc/rc.local
+    sed -i "/echo \$(cat \/etc\/mazi\/mazi.conf | jq /a #RESTART MAZI-USERS SERVICE" /etc/rc.local
+    sed -i "/#RESTART MAZI-USERS SERVICE/a sudo rm /etc/mazi/users.log" /etc/rc.local
+    sed -i "/sudo rm \/etc\/mazi\/users.log/a sudo touch /etc/mazi/users.log" /etc/rc.local
+    sed -i "/sudo touch \/etc\/mazi\/users.log/a /etc/init.d/mazi-users restart" /etc/rc.local
 }
 
 install_hostap_utils(){
